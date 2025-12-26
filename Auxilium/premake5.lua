d21e74572167711dfbbc9b4ab7721a06d3ce9991
Trevor Rose <trevorrose@outlook.com>
2025-12-26 16:59:26 -0600

    Init

---
+++ Auxilium/premake5.lua
@@ -0,0 +1,91 @@
+project "Auxilium"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "axpch.h"
+	pchsource "src/axpch.cpp"
+
+	files {
+		"src/**.h",
+		"src/**.cpp",
+		"ThirdParty/stb_image/**.h",
+		"ThirdParty/stb_image/**.cpp",
+		"ThirdParty/glm/glm/**.hpp",
+		"ThirdParty/glm/glm/**.inl",
+	}
+
+	defines {
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
+	}
+
+	includedirs {
+		"src",
+		"ThirdParty/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}",
+	}
+
+	links {
+		"GLFW",
+		"Glad",
+		"opengl32.lib",
+
+	}
+	flags { "NoPCH" }
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines {
+		}
+
+		links {
+			"%{Library.WinSock}",
+			"%{Library.WinMM}",
+			"%{Library.WinVersion}",
+			"%{Library.BCrypt}",
+		}
+
+	filter "configurations:Debug"
+		defines "AX_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+		links
+		{
+			"%{Library.ShaderC_Debug}",
+			"%{Library.SPIRV_Cross_Debug}",
+			"%{Library.SPIRV_Cross_GLSL_Debug}"
+		}
+
+	filter "configurations:Release"
+		defines "AX_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+			"%{Library.ShaderC_Release}",
+			"%{Library.SPIRV_Cross_Release}",
+			"%{Library.SPIRV_Cross_GLSL_Release}"
+		}
+
+	filter "configurations:Distribution"
+		defines "AX_DIST"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+			"%{Library.ShaderC_Release}",
+			"%{Library.SPIRV_Cross_Release}",
+			"%{Library.SPIRV_Cross_GLSL_Release}"
+		}
